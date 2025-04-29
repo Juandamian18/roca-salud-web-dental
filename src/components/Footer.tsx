84ebb43913936b3250edb25a268104fb44853729
Juan Damian Lucero <juandamian18@hotmail.com>
2025-04-29 14:14:30 -0300

    quitado internacion

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,14 +22,6 @@ const Footer = () => {
               <MapPin className="w-5 h-5 mr-2 mt-0.5 flex-shrink-0" />
               <p>Ambrosetti 698, Caballito, CABA</p>
             </div>
-            <div className="flex items-start mb-3">
-              <MapPin className="w-5 h-5 mr-2 mt-0.5 flex-shrink-0" />
-              <div>
-                <p className="font-semibold">Internaciones</p>
-                <p>Clinica Roca Centenerio</p>
-                <p>Av. Diaz Velez 4792, Caballito</p>
-              </div>
-            </div>
           </div>
           
           <div>
